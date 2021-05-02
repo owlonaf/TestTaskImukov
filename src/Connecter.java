858b1bdc941729589aa45a997b7499d6765e3a62
owlonaf <imviktor0170@gmail.com>
2021-05-03 02:28:02 +0400

    init

--- src/Connecter.java
+++ src/Connecter.java
@@ -1,2 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
 public class Connecter {
+    private String link;
+
+    public Connecter() {
+
+    }
+
+    public Connecter(String link) {
+        this.link = link;
+    }
+
+    public String enterLink(){
+        Scanner scanner = new Scanner(System.in);
+        setLink(scanner.next());
+        return link;
+    }
+
+    public HttpURLConnection httpURLConnection(String link){
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection) new URL(link).openConnection();
+            connection.setRequestMethod("GET");
+            connection.setUseCaches(false);
+            connection.setConnectTimeout(3000);
+            connection.setReadTimeout(3000);
+            connection.connect();
+        } catch (Throwable cause){
+            cause.printStackTrace();
+        } finally {
+            if (connection != null){
+                connection.disconnect();
+            }
+        }
+        return connection;
+    }
+
+    public StringBuilder createText(HttpURLConnection connection) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (HttpURLConnection.HTTP_OK == connection.getResponseCode()){
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String line;
+
+            while ((line = in.readLine()) != null){
+                stringBuilder.append(line);
+                stringBuilder.append("\n");
+            }
+
+        } else {
+            System.out.println("fail: " + connection.getResponseCode() + ", " + connection.getResponseMessage());
+        }
+        return stringBuilder;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
 }
--- src/Delimiter.java
+++ src/Delimiter.java
@@ -1,2 +1,50 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Delimiter {
+    public static String clearingJunk(StringBuilder stringBuilder){
+        String replace = "";
+        String reg = "[a-zA-Z]";
+        String fin = stringBuilder.toString();
+        Pattern pattern = Pattern.compile(reg);
+        Matcher match = pattern.matcher(fin);
+        fin = match.replaceAll(replace);
+        fin = fin.replaceAll("[^а-яА-Я]", "\n");
+        return fin;
+    }
+
+    public static String[] separationString(String str){
+        String[] subStr;
+        String delimeter = "\n+"; // Разделитель
+        subStr = str.split(delimeter); // Разделения строки str с помощью метода split()
+        String[] subStr1;
+        subStr1 = subStr;
+        for (int i = 0; i < subStr.length; i++){
+            subStr1[i] = subStr[i].toLowerCase();
+        }
+        return subStr1;
+    }
+
+    public static HashMap<String, Integer> countingOfStrings(String[] strings){
+        HashMap<String, Integer> counterOfWords = new HashMap<>();
+        for(int i = 0; i < strings.length; i++) {
+            if (counterOfWords.containsKey(strings[i]) == false){
+                counterOfWords.put(strings[i], 1);
+            } else {
+                int v = counterOfWords.get(strings[i]);
+                counterOfWords.put(strings[i], v+1);
+            }
+        }
+        return counterOfWords;
+    }
+
+    public static void countOfWordsOut(HashMap<String, Integer> hashMap){
+        for (Map.Entry entry: hashMap.entrySet()) {
+
+            System.out.println(entry);
+
+        }
+    }
 }
