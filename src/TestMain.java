73d93215b6dd706b72f47ce0e00047eaa61d1770
owlonaf <imviktor0170@gmail.com>
2021-05-12 22:11:26 +0400

    init

--- src/TestMain.java
+++ src/TestMain.java
@@ -3,7 +3,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-//TODO: создать класс с методом сохраняющим мапу в файлик на диске
 //TODO: сделать бдшку для статистики
 public class TestMain {
     public static void main(String[] args) throws IOException {
